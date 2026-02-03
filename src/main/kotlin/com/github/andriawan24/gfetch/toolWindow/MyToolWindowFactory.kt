5964c09308b5e38d724ac9d6e49f2e641848e7b2
GitHub Action <action@github.com>
2026-02-03 04:17:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/andriawan24/gfetch/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/andriawan24/gfetch/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.andriawan24.gfetch.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.andriawan24.gfetch.MyBundle
+import com.github.andriawan24.gfetch.services.MyProjectService
 import javax.swing.JButton
 
 
